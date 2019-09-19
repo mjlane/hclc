13919203166e081580d5018d2aed25e0a7a72582
Mariah Lane <mlane@mg.k12.mo.us>
2019-09-19 14:11:58 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include_once('header.html'); ?>
+<div id="bodyContainer">
+	<div id="leftMidDiv">
+		<div id="sections">
+		<h1>About Us</h1>
+		<p>Pastor's little blurb.</p>
+		<h1>Services</h1>
+		<p>Sunday:<br>
+		Worship Service at 8:00 a.m.<br>
+		Sunday School at 9:30 a.m.<br>
+		<br>
+		Thursday:<br>
+		Adult Bible Class at 6:00 p.m.</p>
+		</div>
+	</div>
+	<div id="rightMidDiv">
+		<div id="banner">
+		<?php include_once('banner/banner.html'); ?>
+		</div>
+	</div>
+</div>
+</body>
---
+++ officers.php
@@ -0,0 +1,16 @@
+<?php include_once('header.html'); ?>
+
+<div id="bodyContainer">
+	<div id="sections">
+		<h1>Officers</h1>
+		<p>President - Terry Jeep<br>
+		Vice President - Johnny Record<br>
+		Secretary - Kaitlyn Ehlers<br>
+		Treasurer - Angelika Record<br>
+		Altar Guild - Jessie Townley<br>
+		Lydia Circle - Ellen Allen<br>
+		Sunday School - Cyndi Flanagan<br>
+		Trustees - Ron Counts, Phil Bailey, Ernie Ehlers<br>
+		Elders - Richard Record, Clint Schwarz, Keith Degner</p>
+	</div>
+</div>
