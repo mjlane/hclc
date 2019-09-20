6bc4f050a121decbd71abdf4eeef5dc221f938ce
Mariah Lane <mlane@mg.k12.mo.us>
2019-09-20 13:30:42 -0500

    Added sundayschool.php.

---
+++ sundayschool.php
@@ -0,0 +1,21 @@
+<?php include_once('header.html'); ?>
+
+<div id="bodyContainer">
+	<div id="sections">
+		<h1>Early Childhood</h1>
+		<p>Our Early Childhood class has children ages 2 to 6 years old.</p>
+
+		<h1>Middle Grades</h1>
+		<p>Our Middle Grades class has children ages 7 to 12 years old.</p>
+
+		<h1>Upper Grades</h1>
+		<p>Our Upper Grades class has children 13 years old and up.</p>
+
+		<h1>Adult Bible Class</h1>
+		<p>blah blah blah</p>
+
+	</div>
+
+<?php include_once('footer.html'); ?>
+
+</div>
