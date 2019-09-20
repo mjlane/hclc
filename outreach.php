141b9ca90cbfda75a6cea96b908cf747e805af1d
Mariah Lane <mlane@mg.k12.mo.us>
2019-09-20 13:29:35 -0500

    Added outreach.php.

---
+++ outreach.php
@@ -0,0 +1,16 @@
+<?php include_once('header.html'); ?>
+
+<div id="bodyContainer">
+	<div id="sections">
+		<h1>Outreach</h1>
+		<p>The Outreach Committee was originally established in Summer 2014 to replace the Evangelism Committee. Their first event was a meal for deer hunters with door prizes held November 2014. Shortly thereafter, the community outreach morphed into a community-wide free dinner on the last Thursday of each month. That year, the last Thursday in November was Thanksgiving and this led to a community-wide Thanksgiving Dinner.<br>
+		Other events participated in by the Outreach Committee include annual Halloween events in Houston, the Christmas Parade, Taste of Houston during the holidays, "Carry the Cross" on Good Friday, and various other events in the community.<br></p>
+
+		<h1>Community Dinner Menus</h1>
+		<p>October 31st:<br>
+		I don't know what we are serving, but I know we are having a trunk or treat for the kids!</p>
+	</div>
+
+<?php include_once('footer.html'); ?>
+
+</div>
