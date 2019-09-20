566f3d1cc3bbb7178fed510914f999d5c22f015b
Mariah Lane <mlane@mg.k12.mo.us>
2019-09-20 13:27:08 -0500

    Added committees.php.

---
+++ committees.php
@@ -0,0 +1,31 @@
+<?php include_once('header.html'); ?>
+
+<div id="bodyContainer">
+	<div id="sections">
+		<h1>Lydia Circle</h1>
+		<p>In the beginning, the ladies' organization was known as Mary Martha Guild. It was led by Alice Loesch, Margaret Hiethold, and several others. During their tenure, the ladies met and made quilts and cooked apple butter in a big kettle, canned it, and sold it as a fundraiser. Later, Joan Reeder led the organization, then Liz Short, and currently, Ellen Allen.<br><br>
+		Currently, the ladies continue to sew twice a month, making quilts for newlyweds in the congregation and for local charities. They have made donations of quilts to support St. Jude's Hospital, Pregnancy Resource Center, Agape House, Orphan Train, and others. Also, they make a financial contribution to local charities every Christmas. These funds are earned at their local rummage and bake sale in the Spring and a craft and bake sale in the Fall.<br></p>
+
+		<h1>Christian Soldiers</h1>
+		<p>Christain Soldiers, the men's group, started as part of the Lutheran Laymen's League. As church members deceased over the years, it was no longer able to support the League and was dropped.<br><br>
+		Over time, the church members decided it needed to form the men's worship groups again and thus named themselves Christian Soldiers. The men came together for bible study, breakfast, and to help church members in need.<br><br>
+		As time went on, they decided to expand their services to the community as part of evangelism and to assist those in need. The Christian Soldiers have since helped many church members and community members and have expanded the reach of the church in their local area. They olso do regular maintenance chores needed around the church.<br></p>
+
+		<h1>Thrivent Financial Members</h1>
+		<p>Thrivent Financial has always been a financial partner, giving the church grants as requested by their members to support activities within the church. This has included fundraising efforts for a member, the trail ride supporting St. Jude's Hospital, rummage sales, Christmas donations for seniors, Christmas caroling, and many social meals. In years past, grants from Thrivent have purchased the white tables and chairs in the fellowship hall. Members also receive choice dollars and these can also be allocated to the church. Learn more at <a href="https://www.thrivent.com/">Thrivent.com</a>.<br></p>
+
+		<h1>Animal Barn Mission</h1>
+		<p>Contributions are placed in the bank at the end of the car in the fellowship hall for purchasing animals through the Lutheran World Relief Gifts Program. These animals are donated to under-privileged farmers in other countries. Learn more at <a href="https://lwr.org">LWR.org</a>.<br></p>
+
+		<h1>Mercy Ministries</h1>
+		<p>Through Mercy Ministries, congregation members who find themselves in a financial situation can receive assistance with their needs.<br></p>
+
+		<h1>Seminarian Support</h1>
+		<p>A monthly amount is contributed to a current seminarian in one of the church's seminaries to help with financial needs.<br></p>
+
+		<h1>Recycling Project</h1>
+		<p>Members of the congregation are encouraged to help protect the environment by recycling plastics, papers, aluminum and steel cans. These are donated to Fort Leonardwood. Proceeds benefit the soldiers and their families.<br></p>
+	</div>
+
+<?php include_once('footer.html'); ?>
+</div>
